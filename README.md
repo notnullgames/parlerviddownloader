2637d33b71dd09ae2161b1b2b9bc42ccf55db02d
David Konsumer <konsumer@jetboystudio.com>
2021-01-19 23:59:34 -0800

    simplify with new ideas

--- README.md
+++ README.md
@@ -1,69 +1,16 @@
 # parlerviddownloader
 
-Tools for mirroring parler videos.
+Tools for messing with parler video meta-data
 
 
-You don't really need to do this, because I used this to create [videos.db](https://github.com/notnullgames/parlerviddownloader/releases/download/0.0.0/videos.zip). This will allow you to geocode data (get city/state/country/zip) from a csv file [like this](https://gofile.io/d/7Wg83o). It also has [user-data](https://gofile.io/d/t6M4zx) 
+### create.py
 
+You don't really need to use this, because I used this to create [videos.db](https://github.com/notnullgames/parlerviddownloader/releases/download/0.0.0/videos.zip).
 
-```
-python3 geocode.py CSV_FILE
-```
-
-## Dependencies
-
-- Download and extract the [videos.db](https://github.com/notnullgames/parlerviddownloader/releases/download/0.0.0/videos.zip) file to the same directory.
-- Install "requests": `pip3 install requests`
-
-
-## Usage
-
-
-This will split out all the IDs for a named city:
-
-
-```
-python3 getbycity.py "Portland, Oregon"
-```
-
-I don't have any abbreviations for states, and some are just weird, so you may have to look in the database to figure out the name format, for example this is Washington DC:
-
-```
-python3 getbycity.py "Washington, DC"
-```
-
-
-This will split out all the IDs for a state:
-
-
-```
-python3 getbystate.py Oregon
-```
-
-and `downloadvideos.py` will download a list of incoming IDs (from stdin) into "downloads/" so to put it all together, to get Portland:
-
-```
-python3 getbycity.py "Portland, Oregon" | python3 downloadvideos.py
-```
-
-If you see "Video is gone" it means that it is no longer on S3 (but may have been archived by someone else.)
-
-
-## new
-
-I am working on some new ideas, which eventually will replace this repo, in new/
-
-
-### meta2db.py
-
-Turn [video meta-data](magnet:?xt=urn:btih:1723e27bc79186c4574ff056ddb458d771c26e2f&dn=metadata.tar.gz&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2) (1032523 total videos) into initial sqlite db.
-
-
-### adduser.py
-
-Correlate [user-data](https://gofile.io/d/t6M4zx) to videos
-
-
+- Install deps with `pip3 install -r requirements.txt`.
+- Grab [user_to_videos.csv](https://gofile.io/d/7Wg83o)
+- Grab [video meta-data](magnet:?xt=urn:btih:1723e27bc79186c4574ff056ddb458d771c26e2f&dn=metadata.tar.gz&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2)
+- run `python3 create.py` to generate videos.db
 
 
 ### search.py
---
+++ create.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+"""
+Build sqlite db from meta-data tarball and user-data CSV
+
+You should have metadata.tar.gz and user_to_video.csv
+"""
+
+import sys
+import csv
+import sqlite3
+import tarfile
+import json
+import re
+import datetime
+import dateutil.parser
+import reverse_geocoder as rg
+
+conn = sqlite3.connect('videos.db')
+c = conn.cursor()
+
+c.execute('''
+  CREATE TABLE IF NOT EXISTS videos (
+    id TEXT PRIMARY KEY,
+    longitude FLOAT,
+    latitude FLOAT,
+    altitude FLOAT,
+    time TEXT,
+    country TEXT,
+    state TEXT,
+    city TEXT,
+    username TEXT,
+    displayname TEXT,
+    mime TEXT,
+    width INT,
+    height INT
+  )
+''')
+
+# convert geo location from dms to dd
+def dms_to_dd(d, m, s):
+  return float(d) + float(m)/60 + float(s)/3600
+
+# get a location in decimal array format from text location
+def getLocation(location):
+  loc = re.findall(r'[\d\.]+', location)
+  lat = dms_to_dd(loc[0], loc[1], loc[2])
+  lng = dms_to_dd(loc[3], loc[4], loc[5])
+  if len(loc) == 7:
+    alt = float(loc[6])
+  else:
+    alt = None
+  return [lat, lng, alt]
+
+
+# insert a record into database
+def insert(record, table='videos'):
+  values = []
+  keys =[]
+  qs = []
+  for k, v in record.items():
+    if v != None:
+      keys.append(k)
+      values.append(v)
+      qs.append('?')
+  return c.execute(f"INSERT INTO {table} ({','.join(keys)}) VALUES ({','.join(qs)})", values)
+  
+
+# build a video-to-user-mapping
+usermap = {}
+namemap = {}
+with open('user_to_video.csv', newline='') as csvfile:
+  for i, row in enumerate(csv.reader(csvfile)):
+    if (i!=0):
+      for vid in [r.split('/')[-1].replace('.mp4', '').replace('_small', '') for r in row[2:]]:
+        namemap[vid] = row[0]
+        usermap[vid] = row[1]
+
+# first pass: create the database with meta-data 
+with tarfile.open('metadata.tar.gz') as metaFile:
+  for f in metaFile.getnames():
+    id = f.replace('metadata/meta-', '').replace('.json', '')
+    if id == 'metadata' or id == 'metadata/.aws':
+      continue
+    print(id)
+    d = metaFile.extractfile(f)
+    if d:
+      for meta in json.loads(d.read()):
+        record = {
+          'id': id,
+          'username': usermap.get(id, None),
+          'displayname': namemap.get(id, None),
+          'mime': meta['MIMEType'],
+          'width': meta.get('SourceImageWidth', None),
+          'height': meta.get('SourceImageHeight', None)
+        }
+        d = meta.get('CreateDate', '0000:00:00 00:00:00')
+        if d != '0000:00:00 00:00:00':
+          record['time'] = dateutil.parser.parse(d).isoformat()
+        gps = meta.get('GPSCoordinates', False)
+        if gps:
+          loc = getLocation(gps)
+          record['latitude'] = loc[0]
+          record['longitude'] = loc[1]
+          record['altitude'] = loc[2]
+        insert(record)
+
+# second pass: geocode all of the locations
+rows = [row for row in c.execute("SELECT latitude,longitude,id FROM videos ORDER BY id")]
+geos = rg.search([(row[0], row[1]) for row in rows ])
+
+# third pass: save geocoding
+for r, row in enumerate(rows):
+  g = geos[r]
+  if g['admin1'] == 'Washington, D.C.':
+    g['admin1'] = "DC"
+    g['name'] = "Washington"
+  c.execute("UPDATE videos SET country=?, state=?, city=? WHERE id=?", ( g['cc'], g['admin1'], g['name'], row[2] ))
+  conn.commit()
+  print(g['cc'], g['admin1'], g['name'], row[2])
