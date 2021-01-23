b48cc3baba635a6357d0c9e63945d5e6c0b9193c
David Konsumer <konsumer@jetboystudio.com>
2021-01-22 21:02:06 -0800

    more fault tolerant exif

--- image_exif.py
+++ image_exif.py
@@ -48,31 +48,36 @@ def insert(record, table='images'):
       keys.append(k)
       values.append(v)
       qs.append('?')
-  return c.executemany(f"INSERT INTO {table} ({','.join(keys)}) VALUES ({','.join(qs)})", [values])
+  return c.executemany(f"INSERT INTO {table} ({','.join(keys)}) VALUES ({','.join(qs)}) ON CONFLICT DO NOTHING", [values])
 
 
 for line in open(os.path.join(__dir, 'ddosecrets-parler-images-listing.txt')):
   fname = line.strip().split(' ')[-1]
   with open(os.path.join(IMAGE_DIR, fname), 'rb') as image_file:
-    image = Image(image_file)
     id = fname.split('.')[0]
     print(id)
     record = {
       'id': id,
       'mime': mimetypes.guess_type(fname)[0]
     }
-    if (image.has_exif):
-      lat = image.get('gps_latitude', None)
-      lng = image.get('gps_longitude', None)
-      if (lat and lng):
-        record['latitude'] = dms_to_dd(lat[0], lat[1], lat[2])
-        record['longitude'] = dms_to_dd(lng[0], lng[1], lng[2])
+    
+    try:
+      image = Image(image_file)
+      if (image.has_exif):
+        lat = image.get('gps_latitude', None)
+        lng = image.get('gps_longitude', None)
+        if (lat and lng):
+          record['latitude'] = dms_to_dd(lat[0], lat[1], lat[2])
+          record['longitude'] = dms_to_dd(lng[0], lng[1], lng[2])
 
-      time = image.get('datetime', None)
-      if time:
-        record['time'] = dateutil.parser.parse(time).isoformat()
-      record['width'] = image['pixel_x_dimension']
-      record['height'] = image['pixel_y_dimension']
+        time = image.get('datetime', None)
+        if time:
+          record['time'] = dateutil.parser.parse(time).isoformat()
+        record['width'] = image['pixel_x_dimension']
+        record['height'] = image['pixel_y_dimension']
+    except:
+      print("error, moving on")
+      pass
     print(record)
     insert(record)
     conn.commit()
@@ -80,16 +85,13 @@ for line in open(os.path.join(__dir, 'ddosecrets-parler-images-listing.txt')):
 print("adding geocoding info")
 rows = [row for row in c.execute("SELECT latitude,longitude,id FROM images WHERE latitude!=0 ORDER BY id")]
 for row in rows:
-  try:
-    geos = rg.search([(row[0], row[1]) for row in rows ])
-    values = []
-    for r, row in enumerate(rows):
-      g = geos[r]
-      if g['admin1'] == 'Washington, D.C.':
-        g['admin1'] = "DC"
-        g['name'] = "Washington"
-      values.append(( g['cc'], g['admin1'], g['name'], row[2] ))
-    c.executemany("UPDATE images SET country=?, state=?, city=? WHERE id=?", values)
-    conn.commit()
-  except IndexError:
-    pass
+  geos = rg.search([(row[0], row[1]) for row in rows ])
+  values = []
+  for r, row in enumerate(rows):
+    g = geos[r]
+    if g['admin1'] == 'Washington, D.C.':
+      g['admin1'] = "DC"
+      g['name'] = "Washington"
+    values.append(( g['cc'], g['admin1'], g['name'], row[2] ))
+  c.executemany("UPDATE images SET country=?, state=?, city=? WHERE id=?", values)
+  conn.commit()
